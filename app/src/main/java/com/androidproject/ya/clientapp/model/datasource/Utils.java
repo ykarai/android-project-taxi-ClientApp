bbc1f99a24bc734fa7163e203bfc77639ac8b02c
yos707@gmail.com <5813yy665>
2018-11-26 19:27:50 +0200

    תוספת:מחלקת עזר להודועות

---
+++ app/src/main/java/com/androidproject/ya/clientapp/model/datasource/Utils.java
@@ -0,0 +1,25 @@
+package com.androidproject.ya.clientapp.model.datasource;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Utils {
+//    public static void showToast(Context mContext, String message){
+//        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+//    }
+
+
+    public interface Action<T> {
+        void onSuccess(T obj);
+
+        void onFailure(Exception exception);
+
+        void onProgress(String status, double percent);
+    }
+
+    public interface NotifyDataChange<T> {
+        void OnDataChanged(T obj);
+
+        void onFailure(Exception exception);
+    }
+}
